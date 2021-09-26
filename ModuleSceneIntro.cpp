2717783f25b5661807a7b345b7a9c1f1df7d803b
ArnauUstrell <61042923+ArnauUstrell@users.noreply.github.com>
2021-09-26 22:07:48 +0200

    Imgui added
    Imgui added

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -3,7 +3,7 @@
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
 
-ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+ModuleSceneIntro::ModuleSceneIntro(Application * app, bool start_enabled) : Module(app, start_enabled)
 {
 }
 
@@ -18,6 +18,14 @@ bool ModuleSceneIntro::Start()
 
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	ImGui::StyleColorsDark();
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
+	ImGui_ImplOpenGL2_Init();
+
 	return ret;
 }
 
@@ -36,6 +44,45 @@ update_status ModuleSceneIntro::Update(float dt)
 	p.axis = true;
 	p.Render();
 
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplSDL2_NewFrame();
+	ImGui::NewFrame();
+
+	{
+		static float f = 0.0f;
+
+		ImGui::Begin("Do you want to close the engine?");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Checkbox("Debug Window", &show_debug_window);
+		if (ImGui::Button("Exit"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			return UPDATE_STOP;
+		ImGui::End();
+	}
+	if (show_debug_window)
+	{
+		static float f = 0.0f;
+
+		ImGui::Checkbox("Demo Window", &show_demo_window);
+		ImGui::Checkbox("Another Window", &show_another_window);
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+		if (ImGui::Button("Close Debug Window"))
+			show_debug_window = false;
+
+	}
+	if (show_another_window)
+	{
+		ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Another Window"))
+			show_another_window = false;
+		ImGui::End();
+	}
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::Render();
+	glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+	glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
 	return UPDATE_CONTINUE;
 }
-
--- ModuleSceneIntro.h
+++ ModuleSceneIntro.h
@@ -2,6 +2,10 @@
 #include "Module.h"
 #include "p2DynArray.h"
 #include "Globals.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_sdl.h"
+#include "imgui/imgui_impl_opengl2.h"
+#include <gl/GL.h>
 
 class ModuleSceneIntro : public Module
 {
@@ -9,9 +13,15 @@ public:
 	ModuleSceneIntro(Application* app, bool start_enabled = true);
 	~ModuleSceneIntro();
 
+	bool show_demo_window = false;
+	bool show_another_window = false;
+	bool show_debug_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	bool Start();
 	update_status Update(float dt);
 	bool CleanUp();
 
 public:
 };
+
